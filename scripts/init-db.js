333165977445febd40ab000c9e1b7cd1183da2af
LouisLec <lecointre.louis@gmail.com>
2023-01-27 01:21:11 +0100

    💚 add `.node-version` for deploying on render

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -66,12 +66,12 @@ const main = async () => {
   const client = await pgPool.connect();
 
   try {
-    await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME};`);
-    await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}_shadow;`);
-    await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}_test;`);
-    await client.query(`DROP ROLE IF EXISTS ${DATABASE_VISITOR};`);
-    await client.query(`DROP ROLE IF EXISTS ${DATABASE_AUTHENTICATOR};`);
-    await client.query(`DROP ROLE IF EXISTS ${DATABASE_OWNER};`);
+    //await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME};`);
+    //await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}_shadow;`);
+    //await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}_test;`);
+    //await client.query(`DROP ROLE IF EXISTS ${DATABASE_VISITOR};`);
+    //await client.query(`DROP ROLE IF EXISTS ${DATABASE_AUTHENTICATOR};`);
+    //await client.query(`DROP ROLE IF EXISTS ${DATABASE_OWNER};`);
     console.log(
       chalk.bgWhite.gray("The following roles and databases have been dropped:")
     );
