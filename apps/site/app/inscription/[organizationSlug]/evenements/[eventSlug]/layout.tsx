2211a0e0e093eaf6057e237b5b361460b348e1f5
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-07 10:11:01 +0100

    unnecessary pages

--- apps/site/app/inscription/[organizationSlug]/evenements/[eventSlug]/layout.tsx
+++ apps/site/app/inscription/[organizationSlug]/evenements/[eventSlug]/layout.tsx
@@ -23,66 +23,6 @@ export default function AttendeesLayout({
 }) {
   return (
     <>
-      <nav className="flex" aria-label="Breadcrumb">
-        <ol role="list" className="flex space-x-4 rounded-md shadow">
-          <li className="flex">
-            <div className="flex items-center">
-              <ChevronRight className="flex-shrink-0 w-3 h-3" aria-hidden="true" />
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  {" "}
-                  <div className="ml-4 text-sm font-medium underline">{eventSlug}</div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <Link
-                    href={`/inscription/${organizationSlug}/evenements/${eventSlug}`}
-                    className={cn(
-                      "text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                    )}
-                  >
-                    <DropdownMenuItem className="self-center">Mes participants</DropdownMenuItem>
-                  </Link>
-                  <DropdownMenuSeparator />
-                  <Link
-                    href={`/inscription/${organizationSlug}/evenements/${eventSlug}/infos`}
-                    className={cn(
-                      "text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                    )}
-                  >
-                    <DropdownMenuItem className="self-center">
-                      <Cog aria-hidden className="w-4 h-4 mr-4" />
-                      Infos événements
-                    </DropdownMenuItem>
-                  </Link>
-                  <Link
-                    href={`/inscription/${organizationSlug}/evenements/${eventSlug}/participant/create`}
-                    className={cn(
-                      "text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                    )}
-                  >
-                    <DropdownMenuItem className="self-center">
-                      <PlusSquare className="w-4 h-4 mr-4" />
-                      Ajouter un participant
-                    </DropdownMenuItem>
-                  </Link>
-
-                  <Link
-                    href={`/inscription/${organizationSlug}/evenements/${eventSlug}/brandings`}
-                    className={cn(
-                      "text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                    )}
-                  >
-                    <DropdownMenuItem className="self-center">
-                      <Clipboard aria-hidden className="w-4 h-4 mr-4" />
-                      Charte graphique formulaire
-                    </DropdownMenuItem>
-                  </Link>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </li>
-        </ol>
-      </nav>
       <div className="w-full"></div>
 
       {/* </section> */}
--- apps/site/app/inscription/[organizationSlug]/evenements/[eventSlug]/participant/page.tsx
+++ apps/site/app/inscription/[organizationSlug]/evenements/[eventSlug]/participant/page.tsx
@@ -15,9 +15,6 @@ const CreateAttendeePage = async ({ params: { organizationSlug, eventSlug } }) =
         <Link href={`/inscription/${organizationSlug}/evenements/${eventSlug}`}>
           <ArrowBigLeft className="w-6 h-6" />
         </Link>
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Inscription
-        </h1>
       </div>
       <div className="flex items-baseline w-full max-w-3xl gap-2 mx-auto">
         <CreateAttendeeForm {...eventBySlug} />
