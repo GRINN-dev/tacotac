d4fa795f0de14f2a91ee6d70aa9fc1a2f56f9478
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-08 09:39:17 +0100

    button return to events

--- apps/site/app/scanner/event/[eventSlug]/page.tsx
+++ apps/site/app/scanner/event/[eventSlug]/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ArrowBigLeft } from "lucide-react";
 
 import { sdk } from "@/lib/sdk";
 
@@ -11,7 +12,7 @@ const EventSlug = async ({ params: { eventSlug } }) => {
       {events?.edges?.map((e, i) => {
         return (
           <div key={i} className="container">
-            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+            <h1 className="my-4 text-3xl font-extrabold leading-tight tracking-tighter text-center sm:text-3xl md:text-5xl lg:text-6xl">
               {e.node.name}
             </h1>
             <div> {e?.node?.description}</div>
@@ -19,6 +20,11 @@ const EventSlug = async ({ params: { eventSlug } }) => {
             <div>Capacité : {e?.node?.capacity}</div>
             <div>Lieu : {e?.node?.placeName} </div>
             <Link href={``}>Liste des inscrits</Link>
+            <div className="absolute bottom-2">
+              <Link href={"/scanner"} className="flex items-center">
+                <ArrowBigLeft className="w-12 h-12" /> Retour événements
+              </Link>
+            </div>
           </div>
         );
       })}{" "}
--- apps/site/app/scanner/page.tsx
+++ apps/site/app/scanner/page.tsx
@@ -7,7 +7,7 @@ import EventCard from "./eventCard";
 const ScanPage = async () => {
   const { events } = await sdk().GetAllEvents();
   return (
-    <div className="container grid items-center gap-6 pt-6 pb-8 transition-opacity duration-500 bg-gray-100 opacity-100 md:py-10">
+    <div className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
       <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
         Événements
       </h1>
