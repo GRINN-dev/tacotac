a48705e5643a845525f6f1741e62bd3c3a271e7c
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-20 13:40:34 +0100

    🗑️ clean

--- apps/site/app/scanner/event/[eventId]/scan/steps/DisplayingResults.tsx
+++ apps/site/app/scanner/event/[eventId]/scan/steps/DisplayingResults.tsx
@@ -17,17 +17,7 @@ export const DisplayingResults: FC<{ state: State; dispatch: Dispatch<Event> }> 
   const closeModal = () => {
     setIsOpen(false);
   };
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-40%",
-      transform: "translate(-50%, -50%)",
-      borderRadius: "16px",
-    },
-  };
+
   const scanAttendee = () =>
     sdk().ScanAttendee({
       input: {
