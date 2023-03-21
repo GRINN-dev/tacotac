0b6ca90103a82ed4cb6544ed88d8d555b135ca26
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-21 12:13:44 +0100

    ♻️ EventCard

--- apps/site/app/scanner/eventCard.tsx
+++ apps/site/app/scanner/eventCard.tsx
@@ -1,25 +1,17 @@
 "use client";
 
 import { FC } from "react";
+import { GetAllEventsQuery } from "@/../../@tacotacIO/codegen/dist";
+import dayjs from "dayjs";
 
-interface EventCardProps {
-  eventName: string;
-  eventDate: string;
-  eventLocation: string;
-  eventPicture?: string;
-  eventSlug: string;
-  eventId: string;
-}
+interface EventCardProps extends ExtractArrayType<ExtractType<GetAllEventsQuery, "events">, "nodes"> {}
 
-const EventCard: FC<EventCardProps> = ({ eventDate, eventSlug, eventLocation, eventName, eventPicture, eventId }) => {
-  const date = new Date(eventDate);
-
-  const formattedDate = date?.toLocaleDateString("fr-FR");
+const EventCard: FC<EventCardProps> = ({ startsAt, slug, placeName, name, id }) => {
   return (
     <div className="">
       <div className="border rounded-[10px] text-right h-full w-full bg-[#D9D9D9] p-4 my-2 ">
-        <div className="font-semibold text-white">{eventName}</div>
-        {formattedDate} <div>{eventLocation}</div>
+        <div className="font-semibold text-white">{name}</div>
+        {dayjs(startsAt).format("DD/MM/YYYY")} <div>{placeName}</div>
       </div>
     </div>
   );
