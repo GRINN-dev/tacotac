87991e5b3baa8cb23cab56722e00ae23b493166e
LouisLec <lecointre.louis@gmail.com>
2023-03-24 10:55:28 +0100

    copy new auth related sql

---
+++ apps/db/migrations/current/00200-users copy.sql
@@ -0,0 +1,175 @@
+/*
+  TABLE: publ.users
+  DESCRIPTION: Users of the app
+*/
+drop table if exists publ.users cascade;
+create table publ.users (
+    id uuid not null default uuid_generate_v4() primary key unique, 
+    username citext not null unique check(length(username) >= 2 and length(username) <= 24 and username ~ '^[a-zA-Z]([_]?[a-zA-Z0-9])+$'),
+    firstname text not null,
+    lastname text not null,
+    avatar_url text check(avatar_url ~ '^https?://[^/]+'),
+    is_admin boolean not null default false,
+    is_volunteer boolean not null default false,
+    phone_number text, 
+    is_verified boolean not null default false,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
+);
+
+-- indexes
+  create index on publ.users(created_at);
+  create index on publ.users(updated_at);
+  create index on publ.users(username);
+
+-- RBAC
+  grant select on publ.users to :DATABASE_VISITOR; 
+  grant update (firstname, lastname, avatar_url, is_volunteer, username, phone_number) on publ.users to :DATABASE_VISITOR;
+  grant delete on publ.users to :DATABASE_VISITOR;
+
+-- triggers
+  create trigger _100_timestamps
+  before insert or update on publ.users
+  for each row
+  execute procedure priv.tg__timestamps();
+
+-- RLS
+  alter table publ.users enable row level security;
+
+ create policy no_limit /*TODO: update policy*/
+   on publ.users
+   for all
+   using (true)
+   with check(true);
+
+-- fixtures
+  -- fixtures go here
+/*
+  END TABLE: publ.users
+*/
+
+
+  /*
+ * The users table stores (unsurprisingly) the users of our application. You'll
+ * notice that it does NOT contain private information such as the user's
+ * password or their email address; that's because the users table is seen as
+ * public - anyone who can "see" the user can see this information.
+ *
+ * The author sees `is_admin` and `is_verified` as public information; if you
+ * disagree then you should relocate these attributes to another table, such as
+ * `user_secrets`.
+ */
+/* create table publ.users (
+  id uuid primary key default gen_random_uuid(),
+  username citext not null unique check(length(username) >= 2 and length(username) <= 24 and username ~ '^[a-zA-Z]([_]?[a-zA-Z0-9])+$'),
+  name text,
+  avatar_url text check(avatar_url ~ '^https?://[^/]+'),
+  is_admin boolean not null default false,
+  is_verified boolean not null default false,
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+alter table publ.users enable row level security;
+ */
+-- We couldn't implement this relationship on the sessions table until the users table existed!
+alter table priv.sessions
+  add constraint sessions_user_id_fkey
+  foreign key ("user_id") references publ.users on delete cascade;
+
+-- Users are publicly visible, like on GitHub, Twitter, Facebook, Trello, etc.
+create policy select_all on publ.users for select using (true);
+-- You can only update yourself.
+create policy update_self on publ.users for update using (id = publ.current_user_id());
+grant select on publ.users to :DATABASE_VISITOR;
+-- NOTE: `insert` is not granted, because we'll handle that separately
+grant update(username, firstname, lastname, avatar_url) on publ.users to :DATABASE_VISITOR;
+-- NOTE: `delete` is not granted, because we require confirmation via request_account_deletion/confirm_account_deletion
+
+comment on table publ.users is
+  E'A user who can log in to the application.';
+
+comment on column publ.users.id is
+  E'Unique identifier for the user.';
+comment on column publ.users.username is
+  E'Public-facing username (or ''handle'') of the user.';
+comment on column publ.users.avatar_url is
+  E'Optional avatar URL.';
+comment on column publ.users.is_admin is
+  E'If true, the user has elevated privileges.';
+
+/**********/
+
+-- Returns the current user; this is a "custom query" function; see:
+-- https://www.graphile.org/postgraphile/custom-queries/
+-- So this will be queryable via GraphQL as `{ currentUser { ... } }`
+create function publ.current_user() returns publ.users as $$
+  select users.* from publ.users where id = publ.current_user_id();
+$$ language sql stable;
+comment on function publ.current_user() is
+  E'The currently logged in user (or null if not logged in).';
+
+/**********/
+
+-- The users table contains all the public information, but we need somewhere
+-- to store private information. In fact, this data is so private that we don't
+-- want the user themselves to be able to see it - things like the bcrypted
+-- password hash, timestamps of recent login attempts (to allow us to
+-- auto-protect user accounts that are under attack), etc.
+create table priv.user_secrets (
+  user_id uuid not null primary key references publ.users on delete cascade,
+  password_hash text,
+  last_login_at timestamptz not null default now(),
+  failed_password_attempts int not null default 0,
+  first_failed_password_attempt timestamptz,
+  reset_password_token text,
+  reset_password_token_generated timestamptz,
+  failed_reset_password_attempts int not null default 0,
+  first_failed_reset_password_attempt timestamptz,
+  delete_account_token text,
+  delete_account_token_generated timestamptz
+);
+alter table priv.user_secrets enable row level security;
+comment on table priv.user_secrets is
+  E'The contents of this table should never be visible to the user. Contains data mostly related to authentication.';
+
+/*
+ * When we insert into `users` we _always_ want there to be a matching
+ * `user_secrets` entry, so we have a trigger to enforce this:
+ */
+create function priv.tg_user_secrets__insert_with_user() returns trigger as $$
+begin
+  insert into priv.user_secrets(user_id) values(NEW.id);
+  return NEW;
+end;
+$$ language plpgsql volatile set search_path to pg_catalog, public, pg_temp;
+create trigger _500_insert_secrets
+  after insert on publ.users
+  for each row
+  execute procedure priv.tg_user_secrets__insert_with_user();
+comment on function priv.tg_user_secrets__insert_with_user() is
+  E'Ensures that every user record has an associated user_secret record.';
+
+/*
+ * Because you can register with username/password or using OAuth (social
+ * login), we need a way to tell the user whether or not they have a
+ * password. This is to help the UI display the right interface: change
+ * password or set password.
+ */
+create function publ.users_has_password(u publ.users) returns boolean as $$
+  select (password_hash is not null) from priv.user_secrets where user_secrets.user_id = u.id and u.id = publ.current_user_id();
+$$ language sql stable security definer set search_path to pg_catalog, public, pg_temp;
+
+/*
+ * When the user validates their email address we want the UI to be notified
+ * immediately, so we'll issue a notification to the `graphql:user:*` topic
+ * which GraphQL users can subscribe to via the `currentUserUpdated`
+ * subscription field.
+ */
+create trigger _500_gql_update
+  after update on publ.users
+  for each row
+  execute procedure publ.tg__graphql_subscription(
+    'userChanged', -- the "event" string, useful for the client to know what happened
+    'graphql:user:$1', -- the "topic" the event will be published to, as a template
+    'id' -- If specified, `$1` above will be replaced with NEW.id or OLD.id from the trigger.
+  );
