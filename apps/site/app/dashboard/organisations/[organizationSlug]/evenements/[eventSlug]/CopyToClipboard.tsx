a64d9380c3a390aae19b9281848af1d186190479
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-28 11:07:11 +0200

    ♻️ refacto copy code iframe to clipboard

--- apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/CopyToClipboard.tsx
+++ apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/CopyToClipboard.tsx
@@ -6,24 +6,50 @@ import { buttonVariants } from "@/components/ui/button";
 
 const CopyToClipboard: FC = () => {
   const [copied, setCopied] = useState(false);
-
-  const currentUrl = window.location.href;
-  const newUrl = currentUrl.replace("dashboard/organisation", "inscription");
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(`${newUrl}/participant`);
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 2000);
-  };
   return (
-    <div>
-      <button className={buttonVariants({ variant: "outline", size: "lg" })} onClick={() => handleCopy()}>
-        Copier lien iFrame
+    <div className="flex flex-col">
+      <button
+        className={buttonVariants({ variant: "outline", size: "lg", className: "mx-auto" })}
+        onClick={() => {
+          const el = document.getElementById("code-to-copy");
+          navigator.clipboard.writeText(el.innerText).then(() => setCopied(true));
+        }}
+      >
+        {copied ? "Copié !" : "Copier le code"}
       </button>
+      <pre
+        id="code-to-copy"
+        className="px-2 py-4 mt-6 mb-4 overflow-x-auto border rounded-lg border-slate-900 bg-slate-900 dark:border-slate-700 dark:bg-black"
+      >
+        <code className="flex flex-col">
+          <div className="flex">
+            <span className="text-yellow-300 ">&lt;</span>
+            <span className="text-yellow-300">style</span>
+            <span className="text-yellow-300">&gt;</span>
+          </div>
+          <div>
+            <span className="ml-2 text-white">.my-iframe {"{"}</span>
+          </div>
+          <div className="ml-6 text-white">width: 700px;</div>
+          <div className="ml-6 text-white">max-width: 100%;</div>
+          <div className="ml-6 text-white">height: 900px;</div>
+          <div className="ml-6 text-white">border: 1px solid #ccc;</div>
+          <div className="ml-6 text-white">border-radius: 5px;</div>
+          <div className="ml-6 text-white">box-shadow: 0 0 5px #ccc;</div>
+          <div className="ml-2 text-white ">{"}"}</div>
 
-      {copied && <span style={{ color: "green" }}>Copié !</span>}
+          <span className="text-yellow-300"> &lt;/style&gt;</span>
+          <br />
+          <span className="text-yellow-300">&lt;iframe</span>
+          <span className="text-white"> src=&quot;</span>
+          <span className="text-white">
+            https://www.kaypi-app.vercel.app/inscription/the-organisation/evenements/canto/participant&quot;
+          </span>
+
+          <span className="text-white">class=&quot;my-iframe&quot;</span>
+          <span className="text-yellow-300">/&gt;</span>
+        </code>
+      </pre>
     </div>
   );
 };
--- apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/page.tsx
+++ apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { PlusSquare, Send } from "lucide-react";
+import { ClipboardCopyIcon, PlusSquare, Send } from "lucide-react";
 
 import { IData, IHeader, Type, initLimit } from "@/types/filter";
 import { sdk } from "@/lib/sdk";
@@ -68,49 +68,51 @@ const AttendeesPage = async ({
   );
 
   return (
-    <section className="container grid items-center w-full gap-6 pt-6 pb-8 md:py-10">
-      <div className="flex flex-row items-center justify-between w-full max-w-3xl gap-2 mx-auto">
-        <h2 className="pb-2 mt-10 text-3xl font-semibold tracking-tight transition-colors scroll-m-20 first:mt-0 ">
-          Tous les participants
-        </h2>
-        <div className="flex flex-col">
-          {" "}
-          <Link
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-            target="_blank"
-            href={`/inscription/${organizationSlug}/evenements/${eventSlug}/participant`}
-          >
-            iFrame inscription
-          </Link>
-          <CopyToClipboard />
-        </div>
+    <>
+      <section className="container grid items-center w-full gap-6 pt-6 pb-8 md:py-10">
+        <div className="flex flex-row items-center justify-between w-full max-w-3xl gap-2 mx-auto">
+          <h2 className="pb-2 mt-10 text-3xl font-semibold tracking-tight transition-colors scroll-m-20 first:mt-0 ">
+            Tous les participants
+          </h2>
+          <div className="flex flex-col">
+            {" "}
+            <Link
+              className={buttonVariants({ variant: "outline", size: "lg" })}
+              target="_blank"
+              href={`/inscription/${organizationSlug}/evenements/${eventSlug}/participant`}
+            >
+              iFrame inscription
+            </Link>
+          </div>
 
-        <SendAllEmailConfirmDonation eventId={eventBySlug?.id} />
-        <SendAllEmail eventId={eventBySlug?.id} />
-      </div>
-      {flattenedAttendeesFromRegistrations.length > 0 ? (
-        <Collection
-          totalCount={eventBySlug?.registrations?.totalCount}
-          pageInfo={eventBySlug?.registrations?.pageInfo}
-          header={headerAttendees}
-          data={rawAttendees}
-          initLimit={initLimit}
-          isRedirectStop
-        />
-      ) : (
-        <div className="flex flex-col items-start gap-4">
-          <p>
-            Vous n&apos;avez pas encore ajouter de participants <u>ou</u> aucun ne correspondant a votre recherche.
-          </p>
-          <Link
-            href={`/dashboard/organisations/${organizationSlug}/evenements/${eventSlug}/participant/create`}
-            className={buttonVariants({ size: "lg", variant: "outline" })}
-          >
-            <PlusSquare className="w-4 h-4 mr-2" /> Ajouter un participant
-          </Link>
+          <SendAllEmailConfirmDonation eventId={eventBySlug?.id} />
+          <SendAllEmail eventId={eventBySlug?.id} />
         </div>
-      )}
-    </section>
+        {flattenedAttendeesFromRegistrations.length > 0 ? (
+          <Collection
+            totalCount={eventBySlug?.registrations?.totalCount}
+            pageInfo={eventBySlug?.registrations?.pageInfo}
+            header={headerAttendees}
+            data={rawAttendees}
+            initLimit={initLimit}
+            isRedirectStop
+          />
+        ) : (
+          <div className="flex flex-col items-start gap-4">
+            <p>
+              Vous n&apos;avez pas encore ajouter de participants <u>ou</u> aucun ne correspondant a votre recherche.
+            </p>
+            <Link
+              href={`/dashboard/organisations/${organizationSlug}/evenements/${eventSlug}/participant/create`}
+              className={buttonVariants({ size: "lg", variant: "outline" })}
+            >
+              <PlusSquare className="w-4 h-4 mr-2" /> Ajouter un participant
+            </Link>
+          </div>
+        )}
+      </section>
+      <CopyToClipboard />
+    </>
   );
 };
 
