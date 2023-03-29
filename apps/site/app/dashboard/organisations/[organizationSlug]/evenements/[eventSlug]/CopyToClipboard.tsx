0e26082eb11d4b1d9248df9a69ce891efa3dbaa7
LouisLec <lecointre.louis@gmail.com>
2023-03-29 15:36:52 +0200

    jo

--- apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/CopyToClipboard.tsx
+++ apps/site/app/dashboard/organisations/[organizationSlug]/evenements/[eventSlug]/CopyToClipboard.tsx
@@ -27,7 +27,7 @@ export const CopyToClipboard: FC = ({
       box-shadow: 0 0 5px #ccc;
     }
   </style>
-  <iframe src={"https://www.kaypi-app.vercel.app/inscription/${organisationSlug}/${eventSlug}/iframe"} class="my-iframe" />
+  <iframe src={"https://kaypi-app.vercel.app/inscription/${organisationSlug}/${eventSlug}/iframe"} class="my-iframe" />
   `;
 
   // Returns a highlighted HTML string
