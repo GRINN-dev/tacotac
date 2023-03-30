1a08e640a85e296dfbacead9b7ca174ef8a5179e
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-30 10:34:18 +0200

    fetch attendees by event id

--- apps/site/app/scanner/event/[eventId]/participants/page.tsx
+++ apps/site/app/scanner/event/[eventId]/participants/page.tsx
@@ -5,7 +5,26 @@ const AttendeesPage = async ({ params: { eventId } }) => {
     eventId: eventId,
   });
 
-  return <div className="">Attendees de l&apos;événement {event.name}</div>;
+  return (
+    <div className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        Participants de l&apos;événement {event.name}
+      </h1>
+      <div>
+        {event?.registrations.nodes?.map((registration) => {
+          return registration.attendees?.nodes.map((a, i) => {
+            return (
+              <div key={i}>
+                <div>
+                  {a.firstname} {a.lastname}
+                </div>
+              </div>
+            );
+          });
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default AttendeesPage;
