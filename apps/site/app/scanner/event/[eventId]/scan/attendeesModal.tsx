4ab00ce5c13668c04a0bfe1e70250f4ca808f4ce
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2023-03-31 15:16:10 +0200

    toast, style

--- apps/site/app/scanner/event/[eventId]/scan/attendeesModal.tsx
+++ apps/site/app/scanner/event/[eventId]/scan/attendeesModal.tsx
@@ -2,6 +2,7 @@
 
 import { FC, useState } from "react";
 import { Attendee } from "@/../../@tacotacIO/codegen/dist";
+import { toast } from "@/hooks/use-toast";
 import { Dialog, DialogContent, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
 import { PlusCircle, Users } from "lucide-react";
 
@@ -22,17 +23,17 @@ const AttendeesModal: FC<AttendeesModalProps> = ({ eventId, attendees, eventName
         <DialogTrigger>Liste des inscrits</DialogTrigger>
         <DialogPortal>
           <DialogOverlay />
-          <DialogContent className="overflow-hidden overflow-y-auto data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[50vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <DialogContent className="overflow-hidden overflow-y-auto data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[50vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-gradient-to-b from-white to-gray-200 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
             <DialogTitle className="flex items-center justify-start mb-4 text-lg font-semibold font-zenon-bold">
               <Users className="mx-2 text-primary" /> Participants de {eventName}{" "}
             </DialogTitle>
             <fieldset className="mb-[15px] flex flex-col items-start justify-start">
-              <div className="grid w-full">
+              <div className="grid w-full ">
                 {attendees.map((a, i) => {
                   return (
                     <div
                       key={i}
-                      className="p-2 mb-2 border rounded-md border-slate-700"
+                      className="p-2 mb-2 border rounded-md shadow-lg border-slate-700"
                       onClick={() => {
                         if (!a?.email) {
                           return;
@@ -52,6 +53,7 @@ const AttendeesModal: FC<AttendeesModalProps> = ({ eventId, attendees, eventName
                               },
                             },
                           })
+                          .then(() => toast({ title: "✅ Participant scanné !" }))
                           .catch((error) => {
                             console.log(error);
                           });
