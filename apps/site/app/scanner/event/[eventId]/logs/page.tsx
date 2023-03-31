62eb7fddeecf19523ea83f8dd37cc743fc69c5b8
Baptiste <53108208+Bat-m@users.noreply.github.com>
2023-03-31 16:38:31 +0200

    fix sign_code offline

--- apps/site/app/scanner/event/[eventId]/logs/page.tsx
+++ apps/site/app/scanner/event/[eventId]/logs/page.tsx
@@ -2,26 +2,30 @@ import { LogsStatus } from "@/../../@tacotacIO/codegen/dist";
 import dayjs from "dayjs";
 import { AlertTriangle, CheckCircle, XCircle } from "lucide-react";
 
+
+
 import { sdk } from "@/lib/sdk";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const LogsPage = async ({ params: { eventId } }) => {
   const { event } = await sdk().GetEventById({
     eventId: eventId,
   });
+  console.log("🚀 ~ file: page.tsx:11 ~ const{event}=awaitsdk ~ event:", event);
 
   const IconStatus = ({ status }) => {
     switch (status) {
       case LogsStatus.Ok:
-        return <CheckCircle className="w-5 h-5 text-green-400" />;
+        return <CheckCircle className="h-5 w-5 text-green-400" />;
       case LogsStatus.Warning:
       case LogsStatus.WarningEmail:
+      case LogsStatus.WarningSignCode:
       // De cette façon, si status correspond à l'un des trois cas de LogsStatus.Warning, le code exécutera la ligne de code suivante pour afficher l'icône AlertTriangle.
       case LogsStatus.WarningPanel:
-        return <AlertTriangle className="w-5 h-5 text-orange-400" />;
+        return <AlertTriangle className="h-5 w-5 text-orange-400" />;
       case LogsStatus.Error:
-        return <XCircle className="w-5 h-5 text-red-400" />;
+        return <XCircle className="h-5 w-5 text-red-400" />;
       default:
         break;
     }
@@ -36,7 +40,9 @@ const LogsPage = async ({ params: { eventId } }) => {
       case LogsStatus.WarningEmail:
         return <p>{"L'email ne semble pas avoir était renseigné."}</p>;
       case LogsStatus.WarningPanel:
-        return <p>{"aucun numéro de panneau associé."}</p>;
+        return <p>{"Aucun numéro de panneau associé."}</p>;
+    case LogsStatus.WarningSignCode:
+        return <p>{"Probleme de QR Code"}</p>;
       case LogsStatus.Error:
         return "Une erreur est survenu. Contacter l'équipe technique.";
       default:
@@ -45,17 +51,20 @@ const LogsPage = async ({ params: { eventId } }) => {
   };
 
   const SheetPayload = ({ payload }) => {
+    console.log("🚀 ~ file: page.tsx:48 ~ SheetPayload ~ payload:", payload);
     return (
       <Sheet>
         <SheetTrigger>
-          <Button>Payload</Button>
+          <span>
+            <div className={buttonVariants({ size: "lg" }) + " shadow hover:shadow-lg"}>Payload</div>
+          </span>
         </SheetTrigger>
         <SheetContent size="lg">
           <div className="overflow-hidden shadow sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
               <h3 className="text-base font-semibold leading-6 ">Détail du payload</h3>
             </div>
-            <div className="px-4 py-5 border-t border-gray-200 sm:p-0">
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
               <dl className="sm:divide-y sm:divide-gray-200">
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                   <dt className="text-sm font-medium ">Email</dt>
@@ -65,22 +74,18 @@ const LogsPage = async ({ params: { eventId } }) => {
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                   <dt className="text-sm font-medium ">Numéro de panneau</dt>
                   <dd className="mt-1 text-sm sm:col-span-2 sm:mt-0">
-                    {payload?.ticket_payload?.panelNumber || "manquant"}
+                    {payload?.ticket_payload?.panel_number || "manquant"}
                   </dd>
                 </div>
                 <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                   <dt className="text-sm font-medium ">N° de ticket</dt>
-                  <dd className="mt-1 text-sm sm:col-span-2 sm:mt-0">{payload?.ticket_payload?.ticketNumber}</dd>
+                  <dd className="mt-1 text-sm sm:col-span-2 sm:mt-0">{payload?.ticket_payload?.ticket_number}</dd>
                 </div>
                 <div className="flex items-center py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                   <dt className="text-sm font-medium ">Problème de réseau sur cette inscription</dt>
                   <dd className="mt-1 text-sm sm:col-span-2 sm:mt-0">
                     {payload?.is_coming_from_offline_mode ? "oui" : "non"}
                   </dd>
-                </div>
-                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                  <dt className="text-sm font-medium ">{"Id de l'event"}</dt>
-                  <dd className="mt-1 text-sm sm:col-span-2 sm:mt-0">{payload?.ticket_payload?.eventId}</dd>
                 </div>
               </dl>
             </div>
@@ -96,7 +101,7 @@ const LogsPage = async ({ params: { eventId } }) => {
         Logs de {event.name}
       </h1>
 
-      <div className="flex items-baseline w-full max-w-3xl gap-2 mx-auto">
+      <div className="mx-auto flex w-full max-w-3xl items-baseline gap-2">
         <div className="flow-root">
           <ul role="list" className="-mb-8">
             {event?.logsList?.map((log, logId) => (
@@ -105,19 +110,23 @@ const LogsPage = async ({ params: { eventId } }) => {
                   {logId !== event?.logsList.length - 1 ? (
                     <span className="absolute top-10 left-4 -ml-px h-4 w-0.5 bg-gray-200" aria-hidden="true" />
                   ) : null}
-                  <div className="relative flex space-x-3 ">
+                  <div className="relative flex items-center justify-center space-x-3 ">
                     <div>
                       <span className={"flex h-8 w-8 items-center justify-center rounded-full ring-8 ring-transparent"}>
                         <IconStatus status={log.status} />
                       </span>
                     </div>
-                    <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                    <div className="flex min-w-0 flex-1 items-center justify-between space-x-4 ">
                       <div className="flex items-center justify-center space-x-2">
                         <p className="text-sm ">{log.status}</p>
                         <p> - </p>
-                        <p className="text-sm ">{valueStatus(log.status)} </p>
+                        <p className="text-sm ">{log?.payload?.ticket_payload?.email}</p>
+                        <p> - </p>
+                        <p className="text-sm ">
+                          <span>{valueStatus(log.status)} </span>
+                        </p>
                       </div>
-                      <div className="text-sm text-right whitespace-nowrap ">
+                      <div className="whitespace-nowrap text-right text-sm ">
                         <time dateTime={log.updatedAt}>{dayjs(log.updatedAt).format("DD/MM/YYYY - HH:mm:ss")}</time>
                       </div>
                     </div>
