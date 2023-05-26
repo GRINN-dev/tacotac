7728b0dc84b62dbf638a7cfeda174358b876d79b
LouisLec <lecointre.louis@gmail.com>
2023-05-26 11:33:14 +0200

    jo

--- apps/site/app/admin/[organizationSlug]/(evenements)/page.tsx
+++ apps/site/app/admin/[organizationSlug]/(evenements)/page.tsx
@@ -19,8 +19,8 @@ const EventsPage = async ({ params: { organizationSlug = "all" } }) => {
   return (
     <section className="container pb-8 pt-6 md:py-10">
       <h1 className="admin-h1">Tous les évènements</h1>
-      <Link className={cn(buttonVariants())} href={`/admin/${organizationSlug}/create`}>
-        Créer un évènement
+      <Link href={`/admin/${organizationSlug}/create`} className={buttonVariants({ size: "lg", variant: "outline" })}>
+        <PlusSquare className="mr-2 h-4 w-4" /> Créer un évènement
       </Link>
 
       {events?.length > 0 ? (
@@ -32,12 +32,6 @@ const EventsPage = async ({ params: { organizationSlug = "all" } }) => {
           <p>
             Vous n&apos;avez pas encore créé d&apos;évènements <u>ou</u> aucun ne correspondant a votre recherche.
           </p>
-          <Link
-            href={`/admin/${organizationSlug}/create`}
-            className={buttonVariants({ size: "lg", variant: "outline" })}
-          >
-            <PlusSquare className="mr-2 h-4 w-4" /> Créer un évènement
-          </Link>
         </div>
       )}
     </section>
