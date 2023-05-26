a6597e504c1a265fac548434bf09c82f17504bd7
LouisLec <lecointre.louis@gmail.com>
2023-05-26 11:46:41 +0200

    create event if in all

--- apps/site/app/admin/[organizationSlug]/(evenements)/create/page.tsx
+++ apps/site/app/admin/[organizationSlug]/(evenements)/create/page.tsx
@@ -1,13 +1,53 @@
 import Link from "next/link";
-import { ArrowBigLeft } from "lucide-react";
+import { PlusSquare } from "lucide-react";
 
-import { sdk } from "@/lib/sdk";
+import { serverSdk } from "@/lib/server-sdk";
+import { buttonVariants } from "@/components/ui";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card } from "@/components/ui/card";
 import { CreateEventForm } from "./form";
 
 const CreateEventPage = async ({ params: { organizationSlug } }) => {
-  const { organizationBySlug } = await sdk().GetOrganizationBySlug({
+  const { organizationBySlug } = await serverSdk().GetOrganizationBySlug({
     slug: organizationSlug,
   });
+
+  const { currentUser } = await serverSdk().GetCurrentUser();
+
+  if (organizationSlug === "all")
+    return (
+      <main className="container pb-8 pt-6 md:py-10">
+        <h1 className="admin-h1">Choisir une organisation</h1>
+        <div className="grid w-max gap-4">
+          {currentUser?.organizations?.nodes?.map((organization) => (
+            <Link key={organization.organization.id} href={`/admin/${organization.organization.slug}/create`}>
+              <Card className="flex items-center  px-6 py-2">
+                <Avatar className="mr-2 h-5 w-5">
+                  <AvatarImage
+                    src={
+                      organization.organization.logoUrl ||
+                      `https://avatar.vercel.sh/${organization.organization.slug}.png`
+                    }
+                    alt={organization.organization.name}
+                  />
+                  <AvatarFallback>
+                    {organization.organization.name
+                      .split(" ")
+                      .filter((word) => word.length > 1)
+                      .slice(0, 2)
+                      .map((word) => word[0])
+                      .join("")}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <h2 className="text-lg font-bold">{organization.organization.name}</h2>
+                </div>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      </main>
+    );
 
   return (
     <section className="container  pb-8 pt-6 md:py-10">
