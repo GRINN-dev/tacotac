bcf7d47da39c9ab8c78cce173e09fc336e679d7c
LouisLec <lecointre.louis@gmail.com>
2023-06-07 18:18:28 +0200

    fix refreshing org data

--- apps/db/migrations/current/1-current.sql
+++ apps/db/migrations/current/1-current.sql
@@ -1,1 +1,6 @@
 -- Enter migration here
+drop trigger if exists generate_slug_trigger on publ.organizations;
+CREATE TRIGGER generate_slug_trigger
+BEFORE INSERT ON publ.organizations
+FOR EACH ROW
+EXECUTE FUNCTION publ.generate_slug();
