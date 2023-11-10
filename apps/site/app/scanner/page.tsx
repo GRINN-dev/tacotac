ea42b3d677d71bd6d9668d9359a56810695f94df
Baptiste <53108208+Bat-m@users.noreply.github.com>
2023-11-10 11:30:53 +0100

    redirect if not login scanner

--- apps/site/app/scanner/page.tsx
+++ apps/site/app/scanner/page.tsx
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import dayjs from "dayjs";
 import { ArrowLeft, PartyPopper } from "lucide-react";
 
@@ -11,7 +12,6 @@ import { buttonVariants } from "@/components/ui";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-
 const ScanPage = async () => {
   const { currentUser } = await serverSdk().GetCurrentUser();
   const headersList = headers();
@@ -20,19 +20,7 @@ const ScanPage = async () => {
   let isMobileView = userAgent!.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
 
   if (!currentUser) {
-    return (
-      <div className="bg-muted text-muted-foreground flex h-full w-full items-center justify-center">
-        <Card className="px-4 py-2">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Dashboard Administrateur</h1>
-          <p className="mt-4 leading-7">
-            Pour vous connecter au dashboard admin, vous devez être connecté en tant qu&apos;administrateur.
-          </p>
-          <Link href="/login" className={cn(buttonVariants(), "mt-4")}>
-            Me connecter
-          </Link>
-        </Card>
-      </div>
-    );
+    redirect("/login");
   }
 
   const eventsSorted = currentUser.events?.nodes?.sort((a, b) => dayjs(a.startsAt).diff(dayjs(b.startsAt)) || 0);
